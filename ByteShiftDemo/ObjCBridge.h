6bf795690a4eb41fb8c519825e910dbc6f56c60f
Cher Moua <mouacher@pdx.edu>
2017-07-21 13:38:34 -0700

    Added Beacon Framework

---
+++ ByteShiftDemo/ObjCBridge.h
@@ -0,0 +1,17 @@
+//
+//  ObjCBridge.h
+//  ByteShiftDemo
+//
+//  Created by Cher Moua on 7/21/17.
+//  Copyright © 2017 ByteShift. All rights reserved.
+//
+
+/*
+ This file connects the EstimoteSDK.framework to the application
+ */
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
